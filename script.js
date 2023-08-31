6bd68b9bbf174a7ddb5cfe1226b4d86ed170e46e
Gabrielhz1 <gabrielpxg107@gmail.com>
2023-08-31 14:06:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const key = "0cc1a99004b90ddecdc622bc64ff72a7"
+
+
+function colocarDadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp)+"°C"
+
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity+"%"
+
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    
+}
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+
+    colocarDadosNaTela(dados)
+} 
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+
+}
